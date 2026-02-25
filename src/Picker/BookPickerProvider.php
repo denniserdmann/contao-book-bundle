ef24aaf407aa771bbb14dc8cea1b0e3b88f6989b
Dennis Erdmann <dennis@erdmann-freunde.de>
2026-02-25 15:40:14 +0100

    BookPickerProvider: getDcaTable()-Signatur an Contao-5-Interface anpassen
    DcaPickerProviderInterface::getDcaTable() erwartet in Contao 5 einen
    optionalen PickerConfig-Parameter.

--- src/Picker/BookPickerProvider.php
+++ src/Picker/BookPickerProvider.php
@@ -46,7 +46,7 @@ class BookPickerProvider extends AbstractInsertTagPickerProvider implements DcaP
         return $this->isMatchingInsertTag($config);
     }
 
-    public function getDcaTable(): string
+    public function getDcaTable(?PickerConfig $config = null): string
     {
         return 'tl_book';
     }
